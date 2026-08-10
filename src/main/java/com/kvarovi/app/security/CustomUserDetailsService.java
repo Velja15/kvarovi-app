2e189a7cb5b53c553d795575922660ac0352f30d
Veljko Dimitrijević <dimitrijevicveljko04@gmail.com>
2026-08-11 01:54:46 +0200

    Security i JWT

---
+++ src/main/java/com/kvarovi/app/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.kvarovi.app.security;
+
+import com.kvarovi.app.entity.User;
+import com.kvarovi.app.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Korisnik nije pronadjen: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().getName()))
+        );
+    }
+}
